05e4b3ee73f7a8bc62f3bd51ba59c3a24e2bed80
Neil Resnik <neilr22@comcast.net>
2020-01-22 16:42:23 -0500

    warmup code for api ideas

---
+++ assignment1/part2/object.h
@@ -0,0 +1,24 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "stddef.h"
+
+/**
+ * StrList : This class a base object.
+ *
+ * Auther: resnik.n@husky.neu.edu
+ */
+class Object {
+public:
+    virtual ~Object(){};
+    /** Returns true if this is the same object, false otherwise.
+     *
+     * @arg o - The object to compare.
+     */
+    virtual bool equals(Object* o){ return this == o; };
+
+    /** Returns a unique identifier for each object */;
+    virtual size_t hash(){ return reinterpret_cast<size_t>(this); };
+};
+
+#endif
