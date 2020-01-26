5b0a7e185bcbf5b97c1abd17278bcb8ca914f2e2
gentner.ja@husky.neu.edu <gentner.ja@husky.neu.edu>
2020-01-26 15:20:05 -0500

    Make compliant with CwC and assignment specifications

--- submission/part2/array.h
+++ submission/part2/array.h
@@ -2,16 +2,12 @@
 // Created by 王镱霏 on 1/23/20.
 //
 
-#ifndef HW3_ARRAY_H
-#define HW3_ARRAY_H
+#pragma once
 
 #include "object.h"
 
 class Array : public Object{
 public:
-    int _size;
-    int _capacity;
-    char** _array;
 
     Array();
 
@@ -20,50 +16,47 @@ public:
     virtual ~Array();
 
     // Adds an element at the end of the list
-    void append(char* input);
+    virtual void append(Object* input);
 
     //Removes all the elements from the list
-    void clear();
+    virtual void clear();
 
     // Returns the number of elements with the specified value
-    void count(char* val);
+    virtual void count(Object* val);
 
     // gets the index element
-    char* get(int index);
+    virtual Object* get(int index);
 
-    char* &operator[](int index);
-
-    int size();
+    virtual int size();
 
     // Adds an element at the specified position
-    void insert(int index, char *input);
+    virtual void insert(int index, char *input);
 
     // Removes the first item with the specified value
-    char* remove(int index);
+    virtual Object* remove(int index);
 
     // Add the elements of a array, to the end of the current list
-    void extend(int i, Array* arr1);
+    virtual void extend(int i, Array* arr1);
 
     // Returns the index of the first element with the specified value
-    int index(char* input);
+    virtual int index(Object* input);
     
         // 	Removes the element at the specified position
-    char* pop(int index);
+    virtual Object* pop(int index);
 
     // Reverses the order of the list
-    void reverse();
+    virtual void reverse();
 
     // Sorts the list
-    void sort();
+    virtual void sort();
 
     // set the position
-    char* set(int index, char* input);
+    virtual Object* set(int index, Object* input);
 
-    bool equals(Object* o);
+    virtual bool equals(Object* o);
 
-    size_t hash();
+    virtual size_t hash();
 
     virtual void _ensure_size(int required);
 };
 
-#endif //HW3_ARRAY_H
