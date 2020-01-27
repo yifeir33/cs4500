d53e238ce0cd55b425145c6479dd7161e2e97963
gentner.ja@husky.neu.edu <gentner.ja@husky.neu.edu>
2020-01-27 16:08:19 -0500

    Uncomment tests (:

--- submission/part2/test_array.cpp
+++ submission/part2/test_array.cpp
@@ -194,11 +194,11 @@ int main(int argc, char** argv){
     test2();
     test3();
     test4();
-    //test5();
-    //test6();
-    //test7();
-    //test8();
-    //test9();
-    //test10();
-    //test11();
+    test5();
+    test6();
+    test7();
+    test8();
+    test9();
+    test10();
+    test11();
 }
