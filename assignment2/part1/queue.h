c408c2eef7c1781b740851b9ebdb1b3370f443e6
Neil Resnik <neilr22@comcast.net>
2020-01-30 17:00:25 -0500

    Queue passes tests

--- assignment2/part1/queue.h
+++ assignment2/part1/queue.h
@@ -1,8 +1,12 @@
 //lang::CwC
 #pragma once
 
-#include "object.h"
 #include <cstdlib>
+#include <cstring>
+#include <assert.h>
+
+#include "object.h"
+#include "string.h"
 
 /**
  * Queue: Represents a FIFO data structure.
@@ -23,7 +27,7 @@ public:
     // The Constructor
     Queue() {
       _buffer = new Object*[10];
-      _capacity = size;
+      _capacity = 10;
       _front_index = 0;
       _back_index = 0;
       _size = 0;
@@ -53,9 +57,7 @@ public:
     // removes and returns the object at the end of this queue.
     // errors if there is nothing in the queue
     virtual Object* pop() {
-      if(_size == 0) {
-          // TODO
-      }
+      assert(_size > 0);
       Object *output = _buffer[_front_index];
       _buffer[_front_index] = nullptr;
       _front_index = (_front_index + 1) % _capacity;
@@ -66,9 +68,7 @@ public:
     // returns the object at the end of this queue but does not remove it.
     // errors if there is nothing in the queue
     virtual Object* peek() {
-      if(_size == 0) {
-          // TODO
-      }
+      assert(_size > 0);
       return _buffer[_front_index];
     }
 
@@ -80,11 +80,138 @@ public:
     // true if o is a queue and all of the elements of this queue,
     // in the same order, are equal to o's
     virtual bool equals(Object *other) {
-      // TODO
+        Queue *q_ptr = dynamic_cast<Queue *>(other);
+        if(q_ptr && q_ptr->len() == _size){
+            size_t i = _front_index;
+            size_t j = q_ptr->_front_index;
+            size_t count = 0;
+            while(count < _size){
+                if(!_buffer[i]->equals(q_ptr->_buffer[j])){
+                    return false;
+                }
+                i = (i + 1) % _capacity;
+                j = (j + 1) % q_ptr->_capacity;
+                count++;
+            }
+            return true;
+        }
+        return false;
     }
 
     // hash of this queue
     virtual size_t hash() {
-      // TODO
+        size_t hash = this->_size;
+        size_t index = _front_index;
+        size_t count = 0;
+        while(count < _size){
+            hash += _buffer[index]->hash();
+            index = (index + 1) % _capacity;
+            count++;
+        }
+        return hash;
+    }
+};
+
+/**
+ * StrQueue: Represents a FIFO data structure.
+ *        Capable of holding any Object in the given order.
+ *
+ * mutable
+ *
+ * authors: dermer.s@husky.neu.edu && Sarialli.n@northeastern.edu
+ */
+class StrQueue : public Object {
+public:
+    Object **_buffer;
+    size_t _capacity;
+    size_t _front_index;
+    size_t _back_index;
+    size_t _size;
+
+    // The Constructor
+    StrQueue() {
+      _buffer = new Object*[10];
+      _capacity = 10;
+      _front_index = 0;
+      _back_index = 0;
+      _size = 0;
+    }
+
+    // The Destructor
+    virtual ~StrQueue() {
+      delete[] _buffer;
+    }
+
+    // adds the given object to the beginning of this queue.
+    // undefined behavior: the same pointer is pushed more than once or o is this
+    virtual void push(Object* o) {
+      if(_size + 1 >= _capacity){
+          Object **_new_buf = new Object*[_capacity * 2];
+          memcpy(_new_buf, _buffer, sizeof(Object *) * _capacity);
+          _capacity *= 2;
+          delete[] _buffer;
+          _buffer = _new_buf;
+      }
+
+      _buffer[_back_index] = o;
+      _back_index = (_back_index + 1) % _capacity;
+      _size++;
+    }
+
+    // removes and returns the object at the end of this queue.
+    // errors if there is nothing in the queue
+    virtual Object* pop() {
+      assert(_size > 0);
+      Object *output = _buffer[_front_index];
+      _buffer[_front_index] = nullptr;
+      _front_index = (_front_index + 1) % _capacity;
+      _size--;
+      return output;
+    }
+
+    // returns the object at the end of this queue but does not remove it.
+    // errors if there is nothing in the queue
+    virtual Object* peek() {
+      assert(_size > 0);
+      return _buffer[_front_index];
+    }
+
+    // gives the total number of elements in this StrQueue
+    virtual size_t len() {
+      return _size;
+    }
+
+    // true if o is a queue and all of the elements of this queue,
+    // in the same order, are equal to o's
+    virtual bool equals(Object *other) {
+        StrQueue *q_ptr = dynamic_cast<StrQueue *>(other);
+        if(q_ptr && q_ptr->len() == _size){
+            size_t i = _front_index;
+            size_t j = q_ptr->_front_index;
+            size_t count = 0;
+            while(count < _size){
+                if(!_buffer[i]->equals(q_ptr->_buffer[j])){
+                    return false;
+                }
+                i = (i + 1) % _capacity;
+                j = (j + 1) % q_ptr->_capacity;
+                count++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // hash of this queue
+    virtual size_t hash() {
+        size_t hash = this->_size;
+        size_t index = _front_index;
+        size_t count = 0;
+        while(count < _size){
+            hash += _buffer[index]->hash();
+            index = (index + 1) % _capacity;
+            count++;
+        }
+        return hash;
     }
 };
