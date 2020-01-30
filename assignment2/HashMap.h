91cdcf4b542f03abe638db031994df544a5ecb70
Yifei <wang.yifei838@gmail.com>
2020-01-29 20:28:26 -0500

    get updated, and everything should work fine

--- assignment2/HashMap.h
+++ assignment2/HashMap.h
@@ -21,8 +21,12 @@ public:
 
     Object *key_;
     Object *value_;
+    HashPair(){
+        this->key_ = nullptr;
+        this->value_ = nullptr;
+    }
 
-     ~HashPair() {
+    ~HashPair() {
         delete key_;
         delete value_;
     }
@@ -57,12 +61,12 @@ public:
 
     //Removes all the elements from the list
     void clear(){
-        for(int i =0; i< size_+1; i++ ){
+        for(int i =0; i< this->size_+1; i++ ){
 
-            for(int j =0;j <size_+1; j++){
-                delete this->map[i][j];
-                this->map[i][j] = nullptr;
-            }
+//            for(int j =0;j <this->size_+1; j++){
+//                HashPair* hp = this->map[i][j];
+//                delete hp;
+//            }
             delete [] map[i];
 
         }
@@ -83,46 +87,225 @@ public:
         }
         this->_ensure_size(size_ + 1);
         //check capacity enough
-        std::cout<<"okensuresize"<<std::endl;
 //        std::cout<<this->capacity_<<std::endl;
 //        std::cout<<this->map[0][0]->key_<<std::endl;
-//        std::cout<<"here"<<std::endl;
         HashPair *pair = new HashPair(key, value);
 //        std::cout<<"newpair"<<std::endl;
         int hash_val = hash_index(key);
         HashPair **select_data = map[hash_val];
 //        std::cout<<"newpair1"<<std::endl;
-        std::cout<<"capacity now:"<< this->capacity_<<std::endl;
+
         for (int i = 0; i < this->capacity_; i++) {
 //            std::cout<<"newpair2"<<std::endl;
             if (select_data[i] == nullptr) {
 //                std::cout<<"newpair3"<<std::endl;
-                std::cout<<"current i:"<<i<<std::endl;
+//                std::cout<<"current i:"<<i<<std::endl;
                 select_data[i] = pair;
-                std::cout<<"current i:"<<i<<std::endl;
+//                std::cout<<"current i:"<<i<<std::endl;
                 break;
             }
         }
         size_++;
-        std::cout<<"finish??"<<std::endl;
+//        std::cout<<"finish??"<<std::endl;
 
     }
 
     Object *get(Object *key) {
-        Object **keys = this->getKeys();
-        Object **vals = this->getValues();
-        for (int selected = 0; selected < size_; selected++) {
-            if (keys[selected]->equals(key)) {
-                return vals[selected];
+        String* input_key = helper_cast(key);
+        int select_index = hash_index(key);
+        for (int i = 0; i < size_; i++) {
+            if(map[select_index][i] != nullptr){
+                String* temp_str = helper_cast(map[select_index][i]->key_);
+                if(temp_str != nullptr ){
+                    if (input_key->equals(temp_str)) {
+                        return map[select_index][i]->value_;
+                    }
+                }
             }
+
         }
 
         std::cout << "Not able to find Object" << std::endl;
-        exit(1);
+        return nullptr;
 
     }
 
-    void remove(Object *key) {
+    Object* remove(Object *key) {
+        Object* removed;
+        int h_index = hash_index(key);
+        HashPair **select_array = map[h_index];
+        String *key1 = helper_cast(key);
+        if (key1 == nullptr) {
+            std::cout << "this string can not be nullptr!" << h_index << std::endl;
+            exit(1);
+        }
+        if (select_array == nullptr) {
+            std::cout << "this array can not be nullptr!" << h_index << std::endl;
+            exit(1);
+        }
+        int sofar = 0;
+        HashPair *selected = select_array[sofar];
+        while (selected != nullptr) {
+
+            Object *key_temp = selected->key_;
+
+            if (key_temp == nullptr) {
+                std::cout << "this object can not be nullptr!" << h_index << std::endl;
+                exit(1);
+            }
+
+            String *key2 = helper_cast(key_temp);
+
+            if (key1->equals(key2)) {
+                removed = select_array[sofar]->value_;
+                select_array[sofar] = nullptr;
+                break;
+            }
+
+
+            sofar++;
+            selected = select_array[sofar];
+        }
+        size_ = size_ - 1;
+        return removed;
+    }
+
+    size_t size() {
+        return this->size_;
+    }
+
+    Object **getKeys() {
+        Object **keys;
+        int sofar = 0;
+        keys = new Object *[this->size_+1];
+        for (int i = 0; i < capacity_; i++) {
+            if (map[i] != nullptr) {
+
+                for (int j = 0; j < capacity_; j++) {
+
+                    if (sofar < size_) {
+
+                        HashPair* cur = map[i][j];
+                        if(cur != nullptr){
+                            keys[sofar] = cur->key_;
+                        }
+
+
+                    }
+                    sofar++;
+                }
+            } else {
+                std::cout << "map[i] cant be nullptr" << std::endl;
+            }
+        }
+        return keys;
+    }
+
+    Object **getValues() {
+        Object **values;
+        int sofar = 0;
+        values = new Object *[this->size_+1];
+        for (int i = 0; i < capacity_; i++) {
+            if (map[i] != nullptr) {
+                for (int j = 0; j < capacity_; j++) {
+                    if (sofar < size_) {
+
+                        HashPair* cur = map[i][j];
+                        if(cur != nullptr){
+                            values[sofar] = cur->value_;
+                        }
+
+
+                    }
+                    sofar++;
+                }
+            } else {
+                std::cout << "map[i] cant be nullptr" << std::endl;
+            }
+        }
+        return values;
+
+    }
+
+
+    virtual void _ensure_size(int required) {
+
+        if (required >= this->capacity_) {
+
+            int newCap = this->capacity_ * 2;
+            while (required > newCap) {
+                newCap *= 2;
+            }
+
+            //build new
+            HashPair*** new_map = new HashPair** [newCap];
+
+            for (int i = 0; i < newCap; ++i) {
+                HashPair** hp =  new HashPair*[newCap];
+                new_map[i] = hp;
+                for (int j = 0; j < newCap; ++j) {
+                    new_map[i][j] = nullptr;
+                }
+            }
+            //copy from past
+            for (int i = 0; i < capacity_; i++) {
+                for (int j = 0; j < capacity_; j++) {
+
+//                    std::cout<<"current i:"<<i<<std::endl;
+//                    std::cout<<"current j:"<<j<<std::endl;
+
+                    if (map[i][j] != nullptr) {
+
+                        int index_i = map[i][j]->key_->hash() % (newCap - 1);
+//                        std::cout<<"index_i:"<< index_i<<std::endl;
+//                          HashPair** subarray2 = new_map[index_i];
+                        for (int k = 0; k < capacity_; k++) {
+                            if (new_map[index_i][k] == nullptr) {
+                                new_map[index_i][k] = map[i][j];
+                            }
+                        }
+
+                    }
+                }
+            }
+
+
+
+// delete old map
+            for (int k = 0; k < capacity_; k++) {
+
+//                for(int l =0; l < capacity_; l++){
+//                    delete map[k][l];
+//                    map[k][l] = nullptr;
+//                }
+                delete[] map[k];
+            }
+            delete [] map;
+
+
+            this->capacity_ = newCap;
+            this->map = new_map;
+            this->size_ = size();
+
+//            for (int k = 0; k < capacity_; k++) {
+//
+//                for(int l =0; l < capacity_; l++){
+//
+//                    new_map[k][l] = nullptr;
+//                }
+//                delete[] new_map[k];
+//            }
+//            delete [] new_map;
+
+
+        }
+    }
+
+    String *helper_cast(Object *o) {
+        return dynamic_cast<String *>(o);
+    }
+
+    bool has_key(Object* key) {
         int h_index = hash_index(key);
         HashPair **select_array = map[h_index];
         String *key1 = helper_cast(key);
@@ -149,147 +332,13 @@ public:
 
             if (key1->equals(key2)) {
 
-                select_array[sofar] = nullptr;
-                break;
+                return true;
             }
-
 
             sofar++;
             selected = select_array[sofar];
         }
-        size_ = size_ - 1;
-    }
-
-    int size() {
-        return this->size_;
-    }
-
-    Object **getKeys() {
-        Object **keys;
-        int sofar = 0;
-        keys = new Object *[size_];
-        for (int i = 0; i < capacity_; i++) {
-            if (map[i] != nullptr) {
-                for (int j = 0; j < capacity_; j++) {
-                    if (sofar < size_) {
-                        keys[sofar] = map[i][j]->key_;
-                    }
-                    sofar++;
-                }
-            } else {
-                std::cout << "map[i] cant be nullptr" << std::endl;
-            }
-        }
-        return keys;
-    }
-
-    Object **getValues() {
-        Object **values;
-        int sofar = 0;
-        values = new Object *[size_];
-        for (int i = 0; i < capacity_; i++) {
-            if (map[i] != nullptr) {
-                for (int j = 0; j < capacity_; j++) {
-                    if (sofar < size_) {
-                        values[sofar] = map[i][j]->value_;
-                    }
-                }
-            } else {
-                std::cout << "map[i] cant be nullptr" << std::endl;
-            }
-        }
-        return values;
-
-    }
-
-    bool isEmpty() {
-        for (int i = 0; i < capacity_; i++) {
-            if (map[i] != nullptr) {
-                HashPair **hp = map[i];
-                for (int j = 0; j < capacity_; j++) {
-                    if (hp[j] != nullptr) {
-                        return false;
-                    }
-                }
-            } else {
-                std::cout << "map[i] cant be nullptr" << std::endl;
-            }
-        }
-        return true;
-
-    }
-
-    virtual void _ensure_size(int required) {
-
-        if (required >= this->capacity_) {
-
-            int newCap = this->capacity_ * 2;
-            while (required > newCap) {
-                newCap *= 2;
-            }
-
-            HashPair*** new_map = new HashPair** [newCap] ;
-            //copy from past
-            std::cout<<"again here100"<<std::endl;
-            for (int i = 0; i < capacity_; i++) {
-                for (int j = 0; j < capacity_; j++) {
-                    std::cout<<"again here1000"<<std::endl;
-                    std::cout<<"current i:"<<i<<std::endl;
-                    std::cout<<"current j:"<<j<<std::endl;
-                    HashPair *cur = map[i][j];
-                    if (cur != nullptr) {
-                        std::cout<<"again here100000"<<std::endl;
-                        int index_i = cur->key_->hash() % (newCap - 1);
-                        std::cout<<"here1000009090"<<std::endl;
-                        if(new_map[index_i] == nullptr){
-                                HashPair** subarray = new HashPair*[newCap];
-                                subarray[0] = cur;
-                                new_map[index_i] = subarray;
-                            } else{
-                            HashPair** subarray2 = new_map[index_i];
-                            for(int k = 0; k< capacity_; k++){
-                                if(subarray2[k] == nullptr){
-                                    subarray2[k] = cur;
-                                }
-                            }
-                        }
-
-                    }
-                }
-            }
-            std::cout<<"here1000001"<<std::endl;
-
-            //fill in the blanks
-            for(int p = 0; p< newCap; p++){
-                if(new_map[p] == nullptr){
-                    new_map[p] = new HashPair*[newCap];
-                    for (int m = 0; m<newCap; m++){
-                        new_map[p][m] = nullptr;
-                    }
-                }
-            }
-//
-// delete old map
-            for (int k = 0; k < capacity_; k++) {
-//                std::cout<<"here1000003"<<std::endl;
-//                for(int l =0; l < capacity_; l++){
-//                    std::cout<<"here1000004"<<std::endl;
-//                        delete map[k][l];
-//                }
-                delete[] map[k];
-            }
-            delete [] map;
-
-
-            this->capacity_ = newCap;
-            this->map = new_map;
-            this->size_ = size();
-            std::cout<<"here1000002sasas"<<std::endl;
-        }
-    }
-
-    String *helper_cast(Object *o) {
-        return dynamic_cast<String *>(o);
+        return false;
     }
 
 
--- assignment2/test-hashmap.cpp
+++ assignment2/test-hashmap.cpp
@@ -5,6 +5,7 @@
 #include "HashMap.h"
 #include "object.h"
 #include "string.h"
+
 void FAIL() { exit(1); }
 
 void OK(const char *m) { std::cout<< m <<std::endl; }
@@ -26,7 +27,7 @@ void test1(HashMap *hashmap) {
 
     std::cout<<hashmap->size()<<std::endl;
     t_true(hashmap->size() == 0);
-    
+
     OK("test1");
 }
 
@@ -37,7 +38,7 @@ void test2(HashMap *hashmap) {
     String* str1 = new String("world");
 //    std::cout<<"here"<<std::endl;
     for(int i =0; i< 100; i++){
-        std::cout<<"current ith round:"<<i<<std::endl;
+//        std::cout<<"current ith round:"<<i<<std::endl;
         hashmap->put(str, str1);
     }
 //    std::cout<<"here1"<<std::endl;
@@ -48,10 +49,23 @@ void test2(HashMap *hashmap) {
 //    std::cout<<"here"<<std::endl;
     OK("test2");
 }
+void test3(HashMap *hashmap){
+    String *str = new String("hello");
+    String *str1 = new String("world");
+    String *str2 = new String("hello1");
+    String *str3 = new String("world1");
 
-int main(int argc, char** argv){
-    HashMap* hashmap = new HashMap();
-    test1(hashmap);
-    test2(hashmap);
-    return 0;
+    hashmap->put(str, str1);
+    hashmap->put(str2, str3);
+
+    t_true(hashmap->get(str2) == str3);
+    t_true(hashmap->get(str) == str1);
+    OK("test3");
+}
+int main() {
+    HashMap HashMap;
+
+    test1(&HashMap);
+    test2(&HashMap);
+    test3(&HashMap);
 }
