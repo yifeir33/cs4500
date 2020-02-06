90d04073447b07917f4e81a9ac7f469403037e2f
Yifei <wang.yifei838@gmail.com>
2020-02-06 17:52:22 -0500

    update graph and script

--- assignment3/part1/performance_visualization.py
+++ assignment3/part1/performance_visualization.py
@@ -7,23 +7,44 @@ import glob
 path = os.getcwd()
 
 fig = plt.figure(figsize=(10,55), dpi= 100)
-# allFiles = glob.glob(path + "/*.csv")
-# frame = pd.DataFrame()
-# list_ = []
-# for file_ in allFiles:
-#     df = pd.read_csv(file_, header =0, index_col= None)
-#     list_.append(df)
-# data = pd.concat(list_)
+allFiles = glob.glob(path + "/*.csv")
+frame = pd.DataFrame()
+list_ = []
+for file_ in allFiles:
+    df = pd.read_csv(file_, header =0, index_col= None)
+    df[' CS4500_Assn1_Part1'] =pd.to_numeric(df[' CS4500_Assn1_Part1'], errors='coerce')
+    list_.append(df[[' a1p1', ' A3_a1_p1', ' CS4500A1P1', ' CS4500_Assn1_Part1',
+       ' sorer', ' swd_assignment_3']])
+    # print(df[' CS4500_Assn1_Part1'])
 
-data = pd.read_csv(path + "/data1.csv", header =0)
+
+
+
+data = pd.concat(list_).groupby(level=0).mean()
+
+print(len(data))
+print(data.columns.values)
+data1 = pd.read_csv(path + "/data1.csv", header =0)
+# data2 = pd.read_csv(path + "/data2.csv", header =0)
+# data3 = pd.read_csv(path + "/data3.csv", header =0)
+# data4 = pd.read_csv(path + "/data4.csv", header =0)
+# data5 = pd.read_csv(path + "/data5.csv", header =0)
+# data6 = pd.read_csv(path + "/data6.csv", header =0)
+# data7 = pd.read_csv(path + "/data7.csv", header =0)
+# data8 = pd.read_csv(path + "/data8.csv", header =0)
+# data9 = pd.read_csv(path + "/data9.csv", header =0)
+# data10 = pd.read_csv(path + "/data10.csv", header =0)
+#
+# list_of_data = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]
+
 
 
 
 header_name = data.columns.values.tolist()
-header_name = header_name[1:]
+# header_name = header_name[1:]
 # print(header_name)
-row_name = data.index
-command_name = data['Command']
+row_name = data1.index
+command_name = data1['Command']
 
 ypos = np.arange(0,len(header_name))
 for i in range(len(data)):
@@ -31,14 +52,24 @@ for i in range(len(data)):
     select_row_data = data.loc[i,header_name]
 
     new_array = np.zeros(len(header_name), np.float)
-    for i in range(len(select_row_data)):
+    for j in range(len(select_row_data)):
         try:
-            k = float(select_row_data[i])
+            k = float(select_row_data[j])
         except ValueError:
-            new_array[i] = np.nan
+            new_array[j] = np.nan
         else:
-            new_array[i] = k
+            new_array[j] = k
 
+    if np.isnan(new_array).any():
+        place =  np.argwhere(np.isnan(new_array))
+        # print('here')
+        # print(place[0,0])
+        del header_name[place[0,0]]
+        new_array = np.delete(new_array, place[0,0])
+        ypos = np.arange(0,len(header_name))
+
+    # print("ypos",len(ypos))
+    # print(len(new_array))
     ax.bar(ypos,new_array, align ='center', label = "seconds")
     ax.set_xticks(ypos)
     ax.set_xticklabels(header_name)
@@ -47,6 +78,10 @@ for i in range(len(data)):
     ax.set_title(command_name[i])
 
     ax.legend(loc = 'best')
+    header_name = data.columns.values.tolist()
+
+    ypos = np.arange(0, len(header_name))
+
 plt.tight_layout()
 plt.savefig('performance_compare.png')
 plt.show()
